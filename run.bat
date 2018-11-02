0add06919727e8889f87ceeb5a9400353c6f9188
TheGhostSpirit <quentin.caritey@outlook.fr>
2018-11-02 11:58:54 +0100

    Reset Hard

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd C:\Users\quent\repos\sql-generator
-gcc -c main.c -o obj/main.o
-gcc -c src/menu.c -o obj/menu.o
-gcc -c src/app.c -o obj/app.o
-gcc obj/main.o obj/menu.o obj/app.o -o bin/main.exe
-@echo on
-bin\main.exe
